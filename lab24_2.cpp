d9cba387b9f2fe98a3b9df8434a4b72e5faa0b9c
vorapat0881 <vorapat_g@cmu.ac.th>
2024-03-01 16:38:26 +0700

    Add data to list

--- lab24_2.cpp
+++ lab24_2.cpp
@@ -24,6 +24,25 @@ int main(){
 	line_up.insert(loc,"Oscar");
 	
 	//Write your code here
+	line_up.push_back("Luffy");
+	line_up.push_back("Sanji");
+	line_up.push_back("Nami");
+	
+	line_up.erase(line_up.begin()); // delete at start
+	line_up.erase(line_up.begin()); // delete at start
+	
+	loc = find(line_up.begin(),line_up.end(),"Sanji");
+	line_up.insert(loc,"Narutu");
+	
+	line_up.insert(line_up.begin(), "Prayath");
+	
+	loc = find(line_up.begin(),line_up.end(),"Bob");
+	line_up.insert(loc,"Tony");
+	
+	line_up.erase(loc); // delete bob
+	line_up.erase(line_up.begin());
+	line_up.erase(line_up.begin());
+	line_up.erase(line_up.begin());
 	
 	printList(line_up);
 		
