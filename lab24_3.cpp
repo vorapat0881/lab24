7f373e5374e3c550b67cb19684bf3a2993f8a308
vorapat0881 <vorapat_g@cmu.ac.th>
2024-03-01 16:37:21 +0700

    Add description of count function.

--- lab24_3.cpp
+++ lab24_3.cpp
@@ -3,7 +3,10 @@
 
 using namespace std;
 
-//Write function count() here.
+/// @brief This function is count distinct value of data.
+/// @param data is int array 
+/// @param number is int number of arrray
+/// @return จำนวนตัวที่ไม่ซ้ำกันใน data array
 int count(int data[], int number){
     set<int> set_of_data; // create set to stores data
     for(int i=0; i< number; i++){ // loop to add (int)data in to set data
@@ -11,6 +14,7 @@ int count(int data[], int number){
     }
     return set_of_data.size(); // send size of set data
 }
+
 int main(){	
 	int data[] = {1,2,4,5,4,8,2,1,2,4,6,1,4,4,4,2,1,0,12}; 
 	
